ea7a8337bc3d1d63fe31c7a226c1be758a8a4091
ngarable <ngarable@iastate.edu>
2025-01-12 02:22:25 -0600

    Basic calculator working

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const container = document.querySelector('.container');
+const display = document.querySelector('#display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "error";
+    }
+}
+
+const container = document.querySelector('#container');
 
 
 function add(a, b) {
@@ -13,12 +32,15 @@ function divide(a,b){
     return a / b;
 }  
 
+function multiply(a,b){
+    return a * b;
+}
 function sum(array) {
     return array.reduce((total, current) => total + current, 0);
   };
   
-function multiply(array) {
-    return array.reduce((product, current) => product * current)
+function product(array) {
+    return array.reduce((product, current) => product * current, 1)
   };
   
 function power(a, b) {
